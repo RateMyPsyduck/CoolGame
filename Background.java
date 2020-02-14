662a687eb94a3782c1098c434181b6d2cafbe4a4
Cibirkaite <cibirkai@egb080000009.lancs.local>
2020-02-14 10:54:09 +0000

    It has some problems with the upper pipe thing but I promise to fix it

--- Background.java
+++ Background.java
@@ -2,22 +2,31 @@ public class Background
 {
     public static void main (String [] args)
     {
+        int max = 750;
+        int min = 100;
+
         GameArena arena = new GameArena(1500, 750);
 
         Rectangle grass = new Rectangle(0, 650, 1500, 100, "GREEN");
 
-        Rectangle[] pipes = new Rectangle[4];
+        Rectangle[] pipes = new Rectangle[8];
 
-        pipes[0] = new Rectangle(1000, 250, 100, 1000, "CYAN"); 
-        pipes[1] = new Rectangle(1500, 250, 100, 1000, "RED"); 
-        pipes[2] = new Rectangle(2000, 250, 100, 1000, "MAGENTA"); 
-        pipes[3] = new Rectangle(2500, 250, 100, 1000, "WHITE"); 
+        pipes[0] = new Rectangle(1000, 250, 100, 650, "CYAN"); 
+        pipes[1] = new Rectangle(1500, 550, 100, 650, "RED"); 
+        pipes[2] = new Rectangle(2000, 150, 100, 650, "MAGENTA"); 
+        pipes[3] = new Rectangle(2500, 400, 100, 650, "WHITE"); 
+
+        pipes[4] = new Rectangle(1000, 0, 100, 650, "CYAN"); 
+        pipes[5] = new Rectangle(1500, 0, 100, 650, "RED"); 
+        pipes[6] = new Rectangle(2000, 0, 100, 650, "MAGENTA"); 
+        pipes[7] = new Rectangle(2500, 0, 100, 650, "WHITE"); 
 
         arena.addRectangle(grass);
 
         for (int i = 0; i < 4; i++)
         {
             arena.addRectangle(pipes[i]);
+            arena.addRectangle(pipes[i+4]);
         }
   
         while(true)
@@ -25,10 +34,15 @@ public class Background
             for(int i = 0; i < 4; i++)
             {
                 pipes[i].setXPosition​(pipes[i].getXPosition() - 5);
+                pipes[i+4].setXPosition​(pipes[i].getXPosition() - 5);
+                
 
                 if (pipes[i].getXPosition() < 0)
                 {
                     pipes[i].setXPosition​(2000);
+                    pipes[i].setYPosition​(Math.random() * (max - min + 1) + min);
+                    pipes[i+4].setXPosition​(2000);
+                    pipes[i+4].setYPosition​(Math.random() * (max - min + 1) + min);
                 }
 
             }
